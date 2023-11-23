2722b5cc3db1e3259cdca83c125231becf4a1eeb
Michael Sevilla <makelsevilla@gmail.com>
2023-11-23 16:51:17 +0800

    modified the page layout in section details.

--- resources/js/Pages/Teacher/SectionDetails.jsx
+++ resources/js/Pages/Teacher/SectionDetails.jsx
@@ -8,7 +8,7 @@ import {
 } from "@/Components/ui/accordion.jsx";
 import CreateSectionSubjectModal from "@/Components/CreateSectionSubjectModal.jsx";
 import { Button } from "@/Components/ui/button.jsx";
-import { Link } from "@inertiajs/react";
+import { Link, router } from "@inertiajs/react";
 import {
     AlertDialog,
     AlertDialogAction,
@@ -20,107 +20,122 @@ import {
 } from "@/Components/ui/alert-dialog.jsx";
 import { useState } from "react";
 import CreateStudentModal from "@/Components/CreateStudentModal.jsx";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/Components/ui/table.jsx";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/Components/ui/dropdown-menu.jsx";
+import { Icons } from "@/Components/Icons.jsx";
 
 export default function SectionDetails({ section }) {
-    console.log(section);
     return (
         <TeacherLayout>
             <PageHeader
                 heading={<span className="capitalize">{section.name}</span>}
             />
             <div className="mt-4">
-                <Accordion type="multiple">
-                    <AccordionItem value="subjects">
-                        <AccordionTrigger>Subjects</AccordionTrigger>
-                        <AccordionContent>
-                            <div className="rounded-lg border">
-                                {section.section_subjects
-                                    .sort((a, b) =>
-                                        a.name.localeCompare(b.name),
-                                    )
-                                    .map((subject) => (
-                                        <div
-                                            key={subject.id}
-                                            className="group p-4 hover:bg-secondary"
-                                        >
-                                            <div className="flex items-center">
-                                                <div>
-                                                    <div className="text-sm font-semibold capitalize text-gray-700">
-                                                        {subject.name}
-                                                    </div>
-                                                    <div className="text-xs text-gray-500">
-                                                        {subject?.teacher?.name}
-                                                    </div>
-                                                </div>
-
-                                                <div className="ml-auto">
-                                                    <SectionSubjectActions
-                                                        sectionSubject={subject}
-                                                    />
-                                                </div>
+                <div className="space-y-2">
+                    <div className="flex items-center">
+                        <div className="text-xl">Subjects</div>
+                        <CreateSectionSubjectModal sectionId={section.id}>
+                            <Button size="sm" className="ml-auto text-xs">
+                                Add subject
+                            </Button>
+                        </CreateSectionSubjectModal>
+                    </div>
+                    <div className="rounded-lg border">
+                        {section.section_subjects
+                            .sort((a, b) => a.name.localeCompare(b.name))
+                            .map((subject) => (
+                                <div
+                                    key={subject.id}
+                                    className="group p-4 hover:bg-secondary"
+                                >
+                                    <div className="flex items-center">
+                                        <div>
+                                            <div className="text-sm font-semibold capitalize text-gray-700">
+                                                {subject.name}
+                                            </div>
+                                            <div className="text-xs text-gray-500">
+                                                {subject?.teacher?.name}
                                             </div>
                                         </div>
-                                    ))}
-                            </div>
 
-                            <div className="mt-4 flex">
-                                <CreateSectionSubjectModal
-                                    sectionId={section.id}
-                                >
-                                    <Button
-                                        size="sm"
-                                        className="ml-auto text-xs"
-                                    >
-                                        Add subject
-                                    </Button>
-                                </CreateSectionSubjectModal>
-                            </div>
-                        </AccordionContent>
-                    </AccordionItem>
+                                        <div className="ml-auto">
+                                            <SectionSubjectActions
+                                                sectionSubject={subject}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                    </div>
+                </div>
 
-                    <AccordionItem value="students">
-                        <AccordionTrigger>Students</AccordionTrigger>
-                        <AccordionContent>
-                            <div className="rounded-lg border">
+                <div className="mt-8 space-y-2">
+                    <div className="mt-4 flex">
+                        <div className="text-xl">Students</div>
+                        <CreateStudentModal sectionId={section.id}>
+                            <Button size="sm" className="ml-auto text-xs">
+                                Add student
+                            </Button>
+                        </CreateStudentModal>
+                    </div>
+                    <div className="rounded-lg border">
+                        <Table>
+                            <TableHeader>
+                                <TableRow className="bg-secondary">
+                                    <TableHead className="whitespace-nowrap">
+                                        Student name
+                                    </TableHead>
+                                    <TableHead className="w-full text-center">
+                                        GWA
+                                    </TableHead>
+                                    <TableHead>Remark</TableHead>
+                                    <TableHead>Actions</TableHead>
+                                </TableRow>
+                            </TableHeader>
+
+                            <TableBody>
                                 {section.students
                                     .sort((a, b) =>
                                         a.name.localeCompare(b.name),
                                     )
                                     .map((student) => (
-                                        <div
+                                        <TableRow
                                             key={student.id}
-                                            className="group p-4 hover:bg-secondary"
+                                            className="group"
                                         >
-                                            <div className="flex items-center">
-                                                <div>
-                                                    <div className="text-sm font-semibold capitalize text-gray-700">
-                                                        {student.name}
-                                                    </div>
+                                            <TableCell>
+                                                <div className="text-sm font-semibold capitalize text-gray-700">
+                                                    {student.name}
                                                 </div>
-
-                                                <div className="ml-auto">
-                                                    <StudentActions
-                                                        student={student}
-                                                    />
-                                                </div>
-                                            </div>
-                                        </div>
+                                            </TableCell>
+                                            <TableCell className="text-center">
+                                                <p>8</p>
+                                                <p>(Very Good)</p>
+                                            </TableCell>
+                                            <TableCell>Passed</TableCell>
+                                            <TableCell>
+                                                <StudentActions
+                                                    student={student}
+                                                />
+                                            </TableCell>
+                                        </TableRow>
                                     ))}
-                            </div>
-
-                            <div className="mt-4 flex">
-                                <CreateStudentModal sectionId={section.id}>
-                                    <Button
-                                        size="sm"
-                                        className="ml-auto text-xs"
-                                    >
-                                        Add student
-                                    </Button>
-                                </CreateStudentModal>
-                            </div>
-                        </AccordionContent>
-                    </AccordionItem>
-                </Accordion>
+                            </TableBody>
+                        </Table>
+                    </div>
+                </div>
             </div>
         </TeacherLayout>
     );
@@ -173,16 +188,19 @@ function StudentActions({ student: { id } }) {
     const [showDeleteAlert, setShowDeleteAlert] = useState(false);
     return (
         <>
-            <div className="hidden items-center group-hover:flex">
-                <Button
-                    onClick={() => setShowDeleteAlert(true)}
-                    variant="ghost"
-                    className="hover:bg-destructive hover:text-destructive-foreground"
-                    size="sm"
-                >
-                    Remove
-                </Button>
-            </div>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="h-8 w-8 p-0">
+                        <Icons.moreVertical className="h-4 w-4" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuItem>View</DropdownMenuItem>
+                    <DropdownMenuItem onSelect={() => setShowDeleteAlert(true)}>
+                        Delete
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
             <AlertDialog
                 open={showDeleteAlert}
                 onOpenChange={setShowDeleteAlert}
